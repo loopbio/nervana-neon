d40e70aa8898368f8cd2e309228edd3855007f3d
santi <sdvillal@gmail.com>
2017-03-07 16:30:55 +0100

    make CPU only

--- run_local_build.sh
+++ run_local_build.sh
@@ -13,11 +13,6 @@ mkdir -p "${logsDir}"
 
 cd ${myDir}
 
-# Build the metapackage tracking the feature (only)
-conda build ${package}-cuda-feature \
-      --output-folder ${localRepoDir} \
-      &> >(tee ${logsDir}/${package}-cuda-feature.log)
-
 # Rerender the main recipe
 conda-smithy rerender &> >(tee ${logsDir}/conda-smithy-rerender.log)
 
@@ -25,21 +20,13 @@ conda-smithy rerender &> >(tee ${logsDir}/conda-smithy-rerender.log)
 dockerScript="${myDir}/ci_support/run_docker_build.sh"
 ${dockerScript} &> >(tee ${logsDir}/${package}.log)
 
-# Build the meta package tracking the feature and installing the main package
-conda build ${package}-cuda \
-      --output-folder ${localRepoDir} \
-      --channel file://${localRepoDir} \
-      &> >(tee ${logsDir}/${package}-cuda.log)
-
 # Fertig
 set +x
 echo "All done, please upload the new artifacts in ${localRepoDir} to anaconda.org like:"
-echo "  " `cat ${logsDir}/${package}-cuda-feature.log | grep "anaconda upload"`
 echo "  " `cat ${logsDir}/${package}.log | grep "anaconda upload"`
-echo "  " `cat ${logsDir}/${package}-cuda.log | grep "anaconda upload"`
 echo "add \"-u {channel}\" to upload to a different organization (e.g. \"-u loopbio\")."
 echo "
-Do not forget to build the CPU version of the package (in the \"cpu\" branch).
+Do not forget to build the GPU version of the package (in the \"cpu\" branch).
 
 --- Fertig ---
 "
